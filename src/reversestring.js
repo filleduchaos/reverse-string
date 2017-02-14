d38970103234a4a68053077abc6507499229607c
Princess-Jewel Essien <retrostereoyak@gmail.com>
2017-02-14 20:44:05 +0100

    Complete implementation of ReverseString

--- src/reversestring.js
+++ src/reversestring.js
@@ -1,17 +1,23 @@
 'use strict';
 
 module.exports = {
+	// This function reverses a passed string.
 	reverseString: function(str) {
 		if (typeof str === 'string') {
+			// Return null for an empty string.
 			if (str.length === 0) {
 				return null;
 			}
 
+			// Split string into array of its component characters
+			// Reverse the array
+			// Join the array into a string
 			var result = str.split("").reverse().join("");
 
+			// Return true if string is a palindrome.
 			if (result === str) {
 				return true;
-			}
+			} else return result; // Return reversed string for normal strings.
 		}
 	}
 }
